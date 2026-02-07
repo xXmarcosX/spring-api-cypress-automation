cc4e700bccbebd5712daeb69da9e69b17bff9eb6
xXmarcosX <marcos.v3567@gmail.com>
2026-02-06 22:16:30 -0300

    initial commit

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  allowCypressEnv: false,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:8080/'
+  },
+});
---
+++ cypress/support/e2e.js
@@ -0,0 +1,3 @@
+import './commands'
+import './noBodyReq'
+import './bodyRequest'
